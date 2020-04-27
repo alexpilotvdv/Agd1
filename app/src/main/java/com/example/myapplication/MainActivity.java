dedb40a0cdf48e90de853d4e9c4ad85aadb2ae13
alexpilot <a41751@gmail.com>
2020-04-27 20:09:58 +0300

    add bluetooth

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -22,11 +23,11 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         guiLayaut.addView(agdView);
         Button leftButton = (Button) findViewById(R.id.leftButton); // находим кнопки
         Button rightButton = (Button) findViewById(R.id.rightButton);
+        Button setButton = (Button) findViewById(R.id.setButton);
 
         leftButton.setOnTouchListener(this); // и добавляем этот класс как слушателя (при нажатии сработает onTouch)
         rightButton.setOnTouchListener(this);
-
-
+        setButton.setOnTouchListener(this);
     }
 
     protected void onDestroy(AgdView agdView ){
@@ -36,6 +37,17 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     }
     public boolean onTouch(View button, MotionEvent motion) {
         switch(button.getId()) { // определяем какая кнопка
+
+            case R.id.setButton:
+                switch (motion.getAction()) { // определяем нажата или отпущена
+                    case MotionEvent.ACTION_DOWN:
+                        Intent intent = new Intent(this, NastrActivity.class);
+                        startActivity(intent);
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+                        break;
+                }
             case R.id.leftButton:
                 switch (motion.getAction()) { // определяем нажата или отпущена
                     case MotionEvent.ACTION_DOWN:
